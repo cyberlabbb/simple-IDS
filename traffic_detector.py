81b290c2973998e4804929101e98fc6f0d6a5c5a
thaovann <lethithaovan2711@gmail.com>
2025-10-07 10:43:14 +0700

    first commit

--- traffic_detector.py
+++ traffic_detector.py
@@ -142,7 +142,6 @@ def packet_callback(packet):
     PACKET_COUNT += 1
 
     feats = extract_features(packet)
-    header = feats["header"]  # Header features
     vector = feats["vector"]  # Payload features
     payload_content = feats["payload_content"]  # Extracted payload content
     src_ip = feats["src_ip"]
@@ -170,7 +169,7 @@ def packet_callback(packet):
     if model is not None:
         try:
             # Combine header and payload features
-            X = np.array([header + vector], dtype=np.float32)
+            X = np.array([vector], dtype=np.float32)
 
             # Apply scaler if provided
             if scaler is not None:
